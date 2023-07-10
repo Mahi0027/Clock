4c9f46b336424c270a41e659ff59580989440daf
Mahipal Singh <32619247+mahipal0015@users.noreply.github.com>
2023-07-10 18:37:21 +0530

    alarm setting options completed.

--- src/components/setting/alarm/SetSnooze.tsx
+++ src/components/setting/alarm/SetSnooze.tsx
@@ -1,19 +1,124 @@
-import React, { useState } from "react";
-import ListItemButton from "@mui/material/ListItemButton";
+import React, { useState, useRef, useEffect } from "react";
+import Button from "@mui/material/Button";
 import ListItemText from "@mui/material/ListItemText";
+import DialogTitle from "@mui/material/DialogTitle";
+import DialogContent from "@mui/material/DialogContent";
+import DialogActions from "@mui/material/DialogActions";
+import Dialog from "@mui/material/Dialog";
+import RadioGroup from "@mui/material/RadioGroup";
+import Radio from "@mui/material/Radio";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import ListItemButton from "@mui/material/ListItemButton";
 import Typography from "@mui/material/Typography";
 
+const options = ["Never"];
+for (let i = 1; i <= 30; i++) {
+    options.push(i + (i == 1 ? " minute" : " minutes"));
+}
 function SetSnooze() {
+    const [open, setOpen] = useState(false);
+    const [value, setValue] = useState(options[2]);
+    const handleClickListItem = () => {
+        setOpen(true);
+    };
+    const handleClose = (newValue?: string) => {
+        setOpen(false);
+
+        if (newValue) {
+            setValue(newValue);
+        }
+    };
     return (
-        <ListItemButton
-            sx={{ pl: 9 }}
-            // onClick={handleClickOpen}
-        >
-            <ListItemText
-                primary={<Typography variant="body1">Snooze length</Typography>}
-                secondary="10 minutes"
+        <>
+            <ListItemButton sx={{ pl: 9 }} onClick={handleClickListItem}>
+                <ListItemText
+                    primary={
+                        <Typography variant="body1">Snooze length</Typography>
+                    }
+                    secondary={value}
+                />
+            </ListItemButton>
+            <ConfirmationDialogRaw
+                id="snooze-length"
+                keepMounted
+                value={value}
+                open={open}
+                onClose={handleClose}
             />
-        </ListItemButton>
+        </>
+    );
+}
+
+export interface ConfirmationDialogRawProps {
+    id: string;
+    keepMounted: boolean;
+    value: string;
+    open: boolean;
+    onClose: (value?: string) => void;
+}
+
+function ConfirmationDialogRaw(props: ConfirmationDialogRawProps) {
+    const { onClose, value: valueProp, open, ...other } = props;
+    const [value, setValue] = useState(valueProp);
+    const radioGroupRef = useRef<HTMLElement>(null);
+
+    useEffect(() => {
+        if (!open) {
+            setValue(valueProp);
+        }
+    }, [valueProp, open]);
+
+    const handleEntering = () => {
+        if (radioGroupRef.current != null) {
+            radioGroupRef.current.focus();
+        }
+    };
+
+    const handleCancel = () => {
+        onClose();
+    };
+
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setValue((event.target as HTMLInputElement).value);
+    };
+
+    useEffect(() => {
+        onClose(value);
+    }, [value]);
+
+    return (
+        <Dialog
+            sx={{ "& .MuiDialog-paper": { width: "80%", maxHeight: 435 } }}
+            maxWidth="xs"
+            TransitionProps={{ onEntering: handleEntering }}
+            open={open}
+            {...other}
+        >
+            <DialogTitle>Snooze length</DialogTitle>
+            <DialogContent dividers>
+                <RadioGroup
+                    ref={radioGroupRef}
+                    aria-label="snoozelength"
+                    name="snoozelength"
+                    value={value}
+                    onChange={handleChange}
+                >
+                    {options.map((option) => (
+                        <FormControlLabel
+                            value={option}
+                            key={option}
+                            control={<Radio />}
+                            label={option}
+                        />
+                    ))}
+                </RadioGroup>
+            </DialogContent>
+            <DialogActions>
+                <Button sx={{ fontWeight: "bold" }} onClick={handleCancel}>
+                    Cancel
+                </Button>
+            </DialogActions>
+        </Dialog>
     );
 }
 
